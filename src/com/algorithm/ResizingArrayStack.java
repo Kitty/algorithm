ac2b0d62a492d6a293f4d918178f3fadd8553537
liuziyang <liuzy@youshi99.com>
2018-07-13 21:39:49 +0800

    JAVA实现动态调整数组的大小

---
+++ src/com/algorithm/ResizingArrayStack.java
@@ -0,0 +1,75 @@
+package com.algorithm;
+import java.util.Iterator;
+
+/**
+ * 《算法》第四版 调整数组大小
+ *
+ * @author 灿夏Summer
+ * @@date 2018-07-12
+ */
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+    private Item[] a = (Item[]) new Object[1];
+    private int N = 0;
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    private void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        for (int i = 0; i < N; i++) {
+            temp[i] = a[i];
+            a = temp;
+        }
+    }
+
+    public void push(Item item) {
+        //判断当数组元素计数变量N与数组的长度相等的时候 那么将数组的长度扩充2倍
+        if (N == a.length) {
+            resize(2 * a.length);
+        }
+        a[N++] = item;
+    }
+
+    public Item pop() {
+        Item item = a[N--];
+        a[N] = null;
+        if (N > 0 && N == a.length / 4) {
+            resize(a.length / 2);
+        }
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return a[--i];
+        }
+
+        @Override
+        public void remove() {
+        }
+
+    }
+
+
+}
+
+
+
