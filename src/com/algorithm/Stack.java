902c887708bf178fd82959968156b13daf8f0be6
liuziyang <liuzy@youshi99.com>
2018-07-14 09:46:17 +0800

    增加Stack类

---
+++ src/com/algorithm/Stack.java
@@ -0,0 +1,67 @@
+package com.algorithm;
+
+import java.util.Iterator;
+
+/**
+ * 下压栈链表实现
+ *
+ * @author lzy
+ * @date 2018-7-14
+ */
+public class Stack<Item> implements Iterable<Item> {
+    private Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public void push(Item item){
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop(){
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            i--;
+            return first.item;
+        }
+
+        @Override
+        public void remove() {
+        }
+
+    }
+
+}
