d5f46f2318f070d81517d2855d47d5b5079c5033
liuziyang <280911198@qq.com>
2018-07-14 20:13:02 +0800

    对类进行修改

--- src/com/algorithm/Bag.java
+++ src/com/algorithm/Bag.java
@@ -36,4 +36,25 @@ public class Bag<Item> implements Iterable<Item> {
     public Iterator<Item> iterator() {
         return null;
     }
+
+    private class ListIterator implements Iterator<Item>{
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current.next != null;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
 }
