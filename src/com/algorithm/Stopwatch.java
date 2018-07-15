07ba9d111014704d65c10d9512af20a48e463be2
liuziyang <280911198@qq.com>
2018-07-15 18:12:06 +0800

    添加计数器类

--- src/com/algorithm/Stopwatch.java
+++ src/com/algorithm/Stopwatch.java
@@ -1,5 +1,9 @@
 package com.algorithm;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.ThreeSum;
+
 /**
  * 一种计时器的抽象数据类型
  *
@@ -13,8 +17,20 @@ public class Stopwatch {
         start = System.currentTimeMillis();
     }
 
-    public double elapsedTime(){
+    public double elapsedTime() {
         long now = System.currentTimeMillis();
         return (now - start) / 1000.0;
+    }
+
+    public static void main(String[] args) {
+        int N = 10000;
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(-10000000, 1000000);
+        }
+        Stopwatch timer = new Stopwatch();
+        int cnt = ThreeSum.count(a);
+        double time = timer.elapsedTime();
+        StdOut.println(cnt + "triples" + time + "seconds");
     }
 }
