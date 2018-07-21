4c830ed860edfca424f72a59d6d3691800c493ff
liuziyang <280911198@qq.com>
2018-07-21 10:51:28 +0800

    排序算法实现

--- src/com/algorithm/Selection.java
+++ src/com/algorithm/Selection.java
@@ -7,4 +7,15 @@ package com.algorithm;
  * @date 2018-7-21
  */
 public class Selection {
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+
+            }
+        }
+    }
+
 }
