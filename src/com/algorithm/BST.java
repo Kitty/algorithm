baf688bd495c9bb57e747ddbd26a82ac32e27a4f
liuziyang <280911198@qq.com>
2018-08-09 21:41:51 +0800

    二叉树查找

--- src/com/algorithm/BST.java
+++ src/com/algorithm/BST.java
@@ -7,16 +7,27 @@ package com.algorithm;
  * @date 2018-8-9
  */
 public class BST<Key extends Comparable<Key>, Value> {
-    private class Node{
+    private Node root;
+
+    private class Node {
         private Key key;
         private Value val;
-        private Node left,right;
+        private Node left, right;
         private int N;
-        public Node(Key key,Value val,int N){
+
+        public Node(Key key, Value val, int N) {
             this.key = key;
             this.val = val;
             this.N = N;
         }
 
+    }
+
+    public int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.N;
+        }
     }
 }
