f0fe09d7a8608eacf628a906140bd4b0dc5b4af4
liuziyang <280911198@qq.com>
2018-08-15 21:09:29 +0800

    基于堆的优先队列

--- src/com/algorithm/MaxPQ.java
+++ src/com/algorithm/MaxPQ.java
@@ -18,23 +18,53 @@ public class MaxPQ<Key extends Comparable<Key>> {
         return N == 0;
     }
 
-    public int size(){
+    public int size() {
         return N;
     }
 
-    public void insert(Key v){
-        pq[++N]= v;
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
 
     }
 
-    private void swim(int k){
-        while (k>1&&less(k/2,k)){
-            k = k/2;
+    public Key delMax(){
+        Key max = pq[1];
+        exch(1,N--);
+        pq[N+1] = null;
+        sink(1);
+        return max;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k < N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) {
+                j++;
+            }
+            if (less(k, j)) {
+                break;
+            }
+            exch(k, j);
+            k = j;
         }
     }
 
 
-    private boolean less(Comparable a, Comparable b){
-        return a.compareTo(b)<0;
+    private boolean less(int a, int b) {
+        return pq[a].compareTo(pq[b]) < 0;
     }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+
 }
