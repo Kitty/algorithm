7894ca1b3286d52ee9edf28194e91e0da4743f7a
liuziyang <280911198@qq.com>
2018-08-15 20:48:26 +0800

    基于堆的优先队列

--- src/com/algorithm/MaxPQ.java
+++ src/com/algorithm/MaxPQ.java
@@ -2,9 +2,39 @@ package com.algorithm;
 
 /**
  * 基于堆的优先队列
+ *
  * @author lzy
  * @date 2018-8-15
  */
 public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N = 0;
 
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public void insert(Key v){
+        pq[++N]= v;
+
+    }
+
+    private void swim(int k){
+        while (k>1&&less(k/2,k)){
+            k = k/2;
+        }
+    }
+
+
+    private boolean less(Comparable a, Comparable b){
+        return a.compareTo(b)<0;
+    }
 }
